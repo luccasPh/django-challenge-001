db05f15e4d6110cfee98fd1b350f51fc4d99ee02
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-05-20 14:08:36 -0300

    Added article model

---
+++ app/article/models.py
@@ -0,0 +1,15 @@
+import uuid
+from django.db import models
+
+
+class Article(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    author = models.ForeignKey("author.Author", on_delete=models.CASCADE)
+    category = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    summary = models.TextField()
+    first_paragraph = models.TextField(verbose_name="firstParagraph")
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
---
+++ app/article/tests/test_models.py
@@ -0,0 +1,98 @@
+from django.test import TestCase
+from django.apps import apps
+
+from ..models import Article
+
+AUTHOR_MODEL = apps.get_model("author", "Author")
+
+
+class ArticleModelTest(TestCase):
+    def setUp(self):
+        self.model = Article
+        self.author = AUTHOR_MODEL.objects.create(name="Test Author Name")
+
+    def test_create_article_successful(self):
+        """Test creating a new article successful"""
+        data = dict(
+            author=self.author,
+            category="Category",
+            title="Article title",
+            summary="This is a summary of the article",
+            first_paragraph="This is the first paragraph of this article",
+            body="Second paragraph. Third paragraph",
+        )
+
+        article = self.model.objects.create(**data)
+        self.assertTrue(article.id)
+        self.assertEqual(article.author, self.author)
+        self.assertEqual(article.category, data["category"])
+        self.assertEqual(article.title, data["title"])
+        self.assertEqual(article.summary, data["summary"])
+        self.assertEqual(article.first_paragraph, data["first_paragraph"])
+        self.assertEqual(article.body, data["body"])
+
+    def test_update_article_successful(self):
+        """Test updating an article successful"""
+        create_data = dict(
+            author=self.author,
+            category="Category",
+            title="Article title",
+            summary="This is a summary of the article",
+            first_paragraph="This is the first paragraph of this article",
+            body="Second paragraph. Third paragraph",
+        )
+        article = self.model.objects.create(**create_data)
+
+        update_data = dict(
+            author=self.author,
+            category="New Category",
+            title="Article new title",
+            summary="This is a new summary of the article",
+            first_paragraph="This is the new first paragraph of this article",
+            body="Second new paragraph. Third new paragraph",
+        )
+        self.model.objects.filter(id=article.id).update(**update_data)
+        article.refresh_from_db()
+        self.assertEqual(article.author, update_data["author"])
+        self.assertEqual(article.category, update_data["category"])
+        self.assertEqual(article.title, update_data["title"])
+        self.assertEqual(article.summary, update_data["summary"])
+        self.assertEqual(article.first_paragraph, update_data["first_paragraph"])
+        self.assertEqual(article.body, update_data["body"])
+
+    def test_retrieve_an_article_successful(self):
+        """Test retrieving an article successful"""
+        data = dict(
+            author=self.author,
+            category="Category",
+            title="Article title",
+            summary="This is a summary of the article",
+            first_paragraph="This is the first paragraph of this article",
+            body="Second paragraph. Third paragraph",
+        )
+        article = self.model.objects.create(**data)
+
+        retrieve_article = self.model.objects.filter(id=article.id).first()
+        self.assertEqual(retrieve_article.id, article.id)
+        self.assertEqual(retrieve_article.author, article.author)
+        self.assertEqual(retrieve_article.category, article.category)
+        self.assertEqual(retrieve_article.title, article.title)
+        self.assertEqual(retrieve_article.summary, article.summary)
+        self.assertEqual(retrieve_article.first_paragraph, article.first_paragraph)
+        self.assertEqual(retrieve_article.body, article.body)
+
+    def test_remove_an_article_successful(self):
+        """Test removing an article successful"""
+        data = dict(
+            author=self.author,
+            category="Category",
+            title="Article title",
+            summary="This is a summary of the article",
+            first_paragraph="This is the first paragraph of this article",
+            body="Second paragraph. Third paragraph",
+        )
+        article = self.model.objects.create(**data)
+
+        self.model.objects.filter(id=article.id).delete()
+        exists = self.model.objects.filter(id=article.id).exists()
+        self.assertFalse(exists)
