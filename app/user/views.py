39c9bf11799d15afcf7b46e4cbd41b083c894d70
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-05-20 12:33:53 -0300

    Refactor user app

--- app/user/views.py
+++ app/user/views.py
@@ -1,12 +1,22 @@
-from rest_framework import generics
+from rest_framework import status
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import SignUpSerializer, LoginSerializer
 
 
-class SignUpUserView(generics.CreateAPIView):
-    serializer_class = SignUpSerializer
+class SignUpUserView(APIView):
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 
 class LoginUserView(ObtainAuthToken):
     serializer_class = LoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
